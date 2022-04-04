8cc29fe75a2529b954441bbb5021f239d5c732d9
zoisper <zoisper@gmail.com>
2022-04-04 18:10:34 +0100

    arrumações

--- 6/src/Main.java
+++ 6/src/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main
 {
     
-    public static void main(){
+    public static void main(String[] args){
         
         Veiculo v = new VeiculoPremium();
         System.out.println(v instanceof Veiculo);
